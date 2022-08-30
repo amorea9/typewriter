89cf226bd5107e318a7d50dc33a78e1f281e36f9
Alessia <a.amore9393@gmail.com>
2022-08-30 22:19:14 +0200

    typewriter half done

---
+++ typewriter.js
@@ -0,0 +1,35 @@
+"use strict";
+window.addEventListener("load", init);
+
+let type2;
+let type;
+let i = 0;
+let character;
+
+function init() {
+  type = document.querySelector("#typewriter").textContent;
+  type2 = document.createElement("h1");
+  type2.classList.add("typewritten");
+  type2.textContent = "";
+  document.querySelector("body").appendChild(type2);
+
+  if (type) {
+    loop();
+  } else {
+    return;
+  }
+
+  //loop the function
+  function loop() {
+    let character = type[i];
+    //print out the letter
+    type2.innerHTML = type2.innerHTML + character;
+    i++;
+    if (i < type.length) {
+      //set delay to the letter
+      setTimeout(loop, 400);
+    }
+  }
+
+  //how to hide the first h1?
+}
